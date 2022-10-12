5956d641b73c15f078f6bb6352b3e8edaadab6eb
Scotch <horsinaround235@gmail.com>
2022-10-12 19:50:39 -0400

    if statements for row color and starting script html

--- index.js
+++ index.js
@@ -21,14 +21,31 @@ $(document).ready(function() {
     function renderTimeSheet(today, events) {
 
         let timeSheet = $("div.container");
+        let hourRow = moment(today).hour(8);
+        timeSheet.empty();
 
         for (let i=1; i<10; i++) {
 
+            let hourClass = "";
+
+            if (today.isBefore(hourRow, "hour")){
+                hourClass = "future"
+            } else if (today.isAfter(hourRow, "hour")) {
+                hourClass = "past"
+            } else {
+                hourClass = "present"
+            }
+
+            let row = $("div").addClass("row");
+            let hourStyle = hourRow.format("ha");
+            timeSheet.append(row)
+            
         }
 
     }
 
-    renderTimeSheet();
+ 
+
     initTimeSheet();
     loadTimeSheet();
     storeTimeSheet();

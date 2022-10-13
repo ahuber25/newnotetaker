2bf14d689bff198f882fb776251b57f37e8171c2
Scotch <horsinaround235@gmail.com>
2022-10-12 20:47:40 -0400

    trying and failing to get what's entered to save. don't know where i'm going wrong

--- index.js
+++ index.js
@@ -8,13 +8,6 @@ $(document).ready(function() {
         renderTimeSheet(today, events);
     }
 
-    function loadTimeSheet() {
-        let hourStorage = JSON.parse(localStorage.getItem("events"));
-        if (hourStorage) {
-            events = hourStorage;
-        }
-    }
-
     let events = {};
 
     let hourlies = moment();
@@ -23,45 +16,53 @@ $(document).ready(function() {
 
         let timeSheet = $("div.container");
         let hourRow = moment(today).hour(8);
-        timeSheet.empty();
+        timeSheet.empty()
 
         for (let i=1; i<10; i++) {
 
             let row = $("<div>").addClass("row");
-            let hourClass = "";
+            let classedUp = "";
+            let hourStyle = hourRow.format("h a");
+            hourRow.add(1, "hour");
+            hourlies = moment();
 
             if (today.isBefore(hourRow, "hour")){
-                hourClass = "future"
+                classedUp = "future"
             } else if (today.isAfter(hourRow, "hour")) {
-                hourClass = "past"
+                classedUp = "past"
             } else {
-                hourClass = "present"
+                classedUp = "present"
             }
-
             
-            let hourStyle = hourRow.format("h a");
             timeSheet.append(row)
-            row.append($("<div>").addClass("col-2 hour").text(hourRow.format("h a")))
-            row.append($("<textarea>").addClass(`col-8 ${hourClass}`).text(events[hourStyle]))
-            row.append($("<button>").addClass("col-2 saveBtn").html("<i class='fas fa-save'></i>").attr("aria-label", "Save").attr("id", hourRow.format("h a")));
+            row.append($("<div>").addClass("col-2 hour").text([hourStyle]))
+            row.append($("<textarea>").addClass(`col-8 ${classedUp}`).text(events[hourStyle]))
+            row.append($("<button>").addClass("col-2 saveBtn").html("<i class='fas fa-save'></i>").attr("aria-label", "Save").attr("id", hourRow[hourStyle]));
 
-            hourRow.add(1, "hour");
-
-            hourlies = moment();
         }
 
     }
 
- 
-
-    initTimeSheet();
-    loadTimeSheet();
-
-    function storeTimeSheet() {
-        localStorage.setItem("events", JSON.stringify(events))
+    function loadTimeSheet() {
+        let hourStorage = JSON.parse(localStorage.getItem("events"));
+        if (hourStorage) {
+            events = hourStorage;
+        }
     }
 
+    loadTimeSheet();
+    initTimeSheet();
+    
 
+    function storeTimeSheet() {
+        localStorage.setItem("events", JSON.stringify(events));
+    };
+
+    $(document).on("click", "button.saveBtn", function (event) {
+        let sheet = event.currentTarget.parentElement.children[1].value;
+        events[event.currentTarget.id] = sheet;
+        storeTimeSheet();
+    });
 
 
 })
